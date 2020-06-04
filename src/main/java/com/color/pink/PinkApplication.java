781fd7bf36a0ad69d963b3fdeaa4607760d45084
HarrisonLee1998 <harrison1998lee@gmail.com>
2020-06-04 19:58:53 +0800

    添加README.md

--- src/main/java/com/color/pink/PinkApplication.java
+++ src/main/java/com/color/pink/PinkApplication.java
@@ -1,5 +1,6 @@
 package com.color.pink;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @MapperScan("com.color.pink.dao")
 @SpringBootApplication
 @EnableScheduling
+@EnableEncryptableProperties
 public class PinkApplication extends SpringBootServletInitializer {
 // public class PinkApplication {
     public static void main(String[] args) {
